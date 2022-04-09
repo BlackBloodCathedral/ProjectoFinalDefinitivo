c8581ab388e39bc67ae1154c60de9b55de87cf5c
BlackBloodCathedral <82421540+BlackBloodCathedral@users.noreply.github.com>
2022-04-08 19:44:14 -0600

    Add files via upload

---
+++ Menu.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace FinalProject
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonCitas = new System.Windows.Forms.Button();
+            this.buttonFacturacion = new System.Windows.Forms.Button();
+            this.buttonUsuarios = new System.Windows.Forms.Button();
+            this.buttonClientes = new System.Windows.Forms.Button();
+            this.buttonMascotas = new System.Windows.Forms.Button();
+            this.Inventario = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 13);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(804, 13);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel3.Location = new System.Drawing.Point(0, 437);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(804, 13);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel4.Location = new System.Drawing.Point(788, 7);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 444);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel5.Location = new System.Drawing.Point(0, 5);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(13, 444);
+            this.panel5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 55);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IMAGEN";
+            // 
+            // ButtonCitas
+            // 
+            this.ButtonCitas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonCitas.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonCitas.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.ButtonCitas.Location = new System.Drawing.Point(78, 191);
+            this.ButtonCitas.Name = "ButtonCitas";
+            this.ButtonCitas.Size = new System.Drawing.Size(183, 50);
+            this.ButtonCitas.TabIndex = 6;
+            this.ButtonCitas.Text = "Citas";
+            this.ButtonCitas.UseVisualStyleBackColor = true;
+            // 
+            // buttonFacturacion
+            // 
+            this.buttonFacturacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFacturacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFacturacion.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonFacturacion.Location = new System.Drawing.Point(535, 191);
+            this.buttonFacturacion.Name = "buttonFacturacion";
+            this.buttonFacturacion.Size = new System.Drawing.Size(183, 50);
+            this.buttonFacturacion.TabIndex = 7;
+            this.buttonFacturacion.Text = "Facturacion";
+            this.buttonFacturacion.UseVisualStyleBackColor = true;
+            // 
+            // buttonUsuarios
+            // 
+            this.buttonUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUsuarios.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonUsuarios.Location = new System.Drawing.Point(78, 286);
+            this.buttonUsuarios.Name = "buttonUsuarios";
+            this.buttonUsuarios.Size = new System.Drawing.Size(183, 50);
+            this.buttonUsuarios.TabIndex = 8;
+            this.buttonUsuarios.Text = "Usuarios";
+            this.buttonUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // buttonClientes
+            // 
+            this.buttonClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClientes.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonClientes.Location = new System.Drawing.Point(535, 286);
+            this.buttonClientes.Name = "buttonClientes";
+            this.buttonClientes.Size = new System.Drawing.Size(183, 50);
+            this.buttonClientes.TabIndex = 9;
+            this.buttonClientes.Text = "Clientes";
+            this.buttonClientes.UseVisualStyleBackColor = true;
+            // 
+            // buttonMascotas
+            // 
+            this.buttonMascotas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMascotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMascotas.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonMascotas.Location = new System.Drawing.Point(310, 191);
+            this.buttonMascotas.Name = "buttonMascotas";
+            this.buttonMascotas.Size = new System.Drawing.Size(183, 50);
+            this.buttonMascotas.TabIndex = 10;
+            this.buttonMascotas.Text = "Mascotas";
+            this.buttonMascotas.UseVisualStyleBackColor = true;
+            // 
+            // Inventario
+            // 
+            this.Inventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Inventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inventario.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Inventario.Location = new System.Drawing.Point(310, 286);
+            this.Inventario.Name = "Inventario";
+            this.Inventario.Size = new System.Drawing.Size(183, 50);
+            this.Inventario.TabIndex = 11;
+            this.Inventario.Text = "Inventario";
+            this.Inventario.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSalir.ForeColor = System.Drawing.Color.Red;
+            this.buttonSalir.Location = new System.Drawing.Point(735, 19);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(47, 23);
+            this.buttonSalir.TabIndex = 12;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.Inventario);
+            this.Controls.Add(this.buttonMascotas);
+            this.Controls.Add(this.buttonClientes);
+            this.Controls.Add(this.buttonUsuarios);
+            this.Controls.Add(this.buttonFacturacion);
+            this.Controls.Add(this.ButtonCitas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonCitas;
+        private System.Windows.Forms.Button buttonFacturacion;
+        private System.Windows.Forms.Button buttonUsuarios;
+        private System.Windows.Forms.Button buttonClientes;
+        private System.Windows.Forms.Button buttonMascotas;
+        private System.Windows.Forms.Button Inventario;
+        private System.Windows.Forms.Button buttonSalir;
+    }
+}
